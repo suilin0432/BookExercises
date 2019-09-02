1604daf5ca4eb6f2984f5eec2699748a503c980e
隋霖 <401569798@qq.com>
2019-09-02 19:38:07 +0800

    create the Exercise7_1 class

--- Exercise7_1.py
+++ Exercise7_1.py
@@ -1,17 +1,102 @@
 from libsvm import *
 # PS: 因为我在libsvm的util的文件里面引入了 svmutil svm commonutil 所有内容所以
 # 直接这么写就行了
-
+import os
 
 '''
-下面是教程中的使用的例子说明
+构建库文件的使用类
 '''
+class Exercise7_1(object):
+    def __init__(self):
+        self.trainLabel = []
+        self.trainData = []
+        self.testLabel = []
+        self.testData = []
+        # 下面是默认的各个参数值
+        self.cost = 1
+        self.svmType = 0
+        self.kernelType = 2
+        self.gamma = 1
+        self.degree = 3
+        self.coef0 = 0
 
+    def setCost(self, cost):
+        assert cost > 0
+        self.cost = cost
+
+    def setSvmType(self, svmType):
+        assert svmType in [0, 1, 2, 3, 4]
+        self.svmType = svmType
+
+    def setKernelType(self, kernelType):
+        assert kernelType in [0, 1, 2, 3, 4]
+        self.kernelType = kernelType
+
+    def setGamma(self, gamma):
+        assert gamma > 0
+        self.gamma = gamma
+
+    def setDegree(self, degree):
+        assert degree > 0
+        self.degree = degree
+
+    def setCoef0(self, coef0):
+        self.coef0 = coef0
+
+    def train(self):
+        # 返回模型 model
+        assert len(self.trainLabel) > 0 and len(self.trainData) > 0 and len(self.trainLabel) == len(self.trainData)
+        # svmType kernelType degree gamma coef0 cost
+        param = "-s {0} -t {1} -d {2} -g {3} -r {4} -c {4}".format(self.svmType, self.kernelType, self.degree, self.gamma, self.coef0, self.cost)
+        model = svm_train(self.trainLabel, self.trainData, param)
+        return model
+
+    def test(self, model):
+        assert isinstance(model, svm_model)
+        assert len(self.testLabel) > 0 and len(self.testData) > 0 and len(self.testLabel) == len(self.testData)
+        pLabel, pAcc, pVal = svm_predict(self.testLabel, self.testData, model)
+        return pLabel, pAcc, pVal
+
+    def loadTrainDataFromFile(self, filePath):
+        assert os.path.exists(filePath)
+        self.trainLabel, self.trainData = svm_read_problem(filePath)
+        assert len(self.trainLabel) == len(self.trainData)
+        assert len(self.trainLabel) > 0
+
+    def loadTestDataFromFile(self, filePath):
+        assert os.path.exists(filePath)
+        self.testLabel, self.testData = svm_read_problem(filePath)
+        assert len(self.testLabel) == len(self.testData)
+        assert len(self.testLabel) > 0
+
+    def setTrainData(self, trainLabel, trainData):
+        assert len(trainLabel) == len(trainData)
+        assert len(trainLabel) > 0
+        self.trainLabel, self.trainData = trainLabel, trainData
+
+    def setTestData(self, testLabel, testData):
+        assert len(testLabel) == len(testData)
+        assert len(testLabel) > 0
+        self.testLabel, self.testData = testLabel, testData
+
+
+trainClass = Exercise7_1()
+trainClass.loadTrainDataFromFile("./svmguide1.txt")
+trainClass.loadTestDataFromFile("./svmguide1.t")
+#
+model = trainClass.train()
+
+'''
+下面是教程中的使用的例子说明 注释掉了
+'''
+'''
 # 读取数据
+# PS: svm_read_problem 还有一个参数是 return_scipy = False 即并不默认返回scipy格式的数据
 yTrain, xTrain = svm_read_problem("./svmguide1.txt")
 yTest, xTest = svm_read_problem("./svmguide1.t")
 # for i in range(len(x)):
 #     print(x[i], y[i])
+'''
 
 '''
 svm_train 的 配置参数说明:
@@ -37,9 +122,10 @@ svm_train 的 配置参数说明:
     10. -e epsilon : 设置终止的条件 (默认是 0.001)
     11. -h shrinking : 设置是否使用shrinking heuristics, 0 or 1 (默认为 1)
     12. -b probability_estimates : 是否使用SVC/SVR模型去进行概率估计 0 or 1 (默认为 0)
-    13. -wi weight : 在 C-SVC 中设置类别 i 的参数 C 为 weight * C (默认是 1)
+    13. -wi weight : 在 C-SVC 中设置类别 i 的参数 C 为 weight * C (默认是 1) (PS: 在处理类别不平衡的数据的时候有用)
     14. -v n : n-fold cross validation mode (要求 n >= 2)
     15. -q : quite mode
+'''
 '''
 m = svm_train(yTrain, xTrain, '-c 4') # 因此这里 -c 4 指的是松弛的那个参数
 p_label, p_acc, p_val = svm_predict(yTest, xTest, m)
@@ -48,3 +134,4 @@ print(p_label)
 # ps: 分类问题的均方误差就是 错误率(因为判断错误的时候得到的误差的平方值总是1)
 print(p_acc)
 print(p_val)
+'''
