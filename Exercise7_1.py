b76886b8f4b0b4190d995a10572d51850bea81a5
隋霖 <401569798@qq.com>
2019-09-04 16:56:24 +0800

    add Exercise7_3.py

--- Exercise7_1.py
+++ Exercise7_1.py
@@ -99,102 +99,102 @@ class Exercise7_1(object):
         return self.testLabel
 
 
+if __name__=='__main__':
+    trainClass = Exercise7_1()
+    trainClass.loadTrainDataFromFile("./svmguide1.txt")
+    trainClass.loadTestDataFromFile("./svmguide1.t")
+    # 1. the origin version -- C = 1 and kernelType = 2 (RBF/Gaussian kernel)
+    model1 = trainClass.train()
+    pLabel1, pAcc1, pVal1 = trainClass.predict(model1)
+    print("The accuracy of the first model (C = 1 and kernelType = 2) is {0}%.".format(pAcc1[0]))
+    # 2. 使用svm_scale函数 并没有找到svm_scale这个函数... 是外面的一个运行文件时svm_scale
+    # 命令1: ./svm-scale -l -1 -u 1 -s ../ExerciseCode_Python/scaleParam ../ExerciseCode_Python/svmguide1.txt > ../ExerciseCode_Python/svmguide1_.txt
+    # 命令2: ./svm-scale -r ../ExerciseCode_Python/scaleParam ../ExerciseCode_Python/svmguide1.t > ../ExerciseCode_Python/svmguide1_.t
+    trainClassScale = Exercise7_1()
+    trainClassScale.loadTrainDataFromFile("./svmguide1_.txt")
+    trainClassScale.loadTestDataFromFile("./svmguide1_.t")
+    model2 = trainClassScale.train()
+    pLabel2, pAcc2, pVal2 = trainClassScale.predict(model2)
+    print("The accuracy of the second model (C = 1 and kernelType = 2 and datas are scaled) is {0}%.".format(pAcc1[0]))
 
-trainClass = Exercise7_1()
-trainClass.loadTrainDataFromFile("./svmguide1.txt")
-trainClass.loadTestDataFromFile("./svmguide1.t")
-# 1. the origin version -- C = 1 and kernelType = 2 (RBF/Gaussian kernel)
-model1 = trainClass.train()
-pLabel1, pAcc1, pVal1 = trainClass.predict(model1)
-print("The accuracy of the first model (C = 1 and kernelType = 2) is {0}%.".format(pAcc1[0]))
-# 2. 使用svm_scale函数 并没有找到svm_scale这个函数... 是外面的一个运行文件时svm_scale
-# 命令1: ./svm-scale -l -1 -u 1 -s ../ExerciseCode_Python/scaleParam ../ExerciseCode_Python/svmguide1.txt > ../ExerciseCode_Python/svmguide1_.txt
-# 命令2: ./svm-scale -r ../ExerciseCode_Python/scaleParam ../ExerciseCode_Python/svmguide1.t > ../ExerciseCode_Python/svmguide1_.t
-trainClassScale = Exercise7_1()
-trainClassScale.loadTrainDataFromFile("./svmguide1_.txt")
-trainClassScale.loadTestDataFromFile("./svmguide1_.t")
-model2 = trainClassScale.train()
-pLabel2, pAcc2, pVal2 = trainClassScale.predict(model2)
-print("The accuracy of the second model (C = 1 and kernelType = 2 and datas are scaled) is {0}%.".format(pAcc1[0]))
+    # 3. 使用linear kernel代替 RBF kernel 即使用 -t 参数为0
+    trainClass.setKernelType(0)
+    model3 = trainClass.train()
+    pLabel3, pAcc3, pVal3 = trainClass.predict(model3)
+    print("The accuracy of the third model (C = 1 and kernelType = 0) is {0}%.".format(pAcc3[0]))
 
-# 3. 使用linear kernel代替 RBF kernel 即使用 -t 参数为0
-trainClass.setKernelType(0)
-model3 = trainClass.train()
-pLabel3, pAcc3, pVal3 = trainClass.predict(model3)
-print("The accuracy of the third model (C = 1 and kernelType = 0) is {0}%.".format(pAcc3[0]))
+    # 4. 将 Cost = 1000 然后使用 RBF kernel
+    trainClass.setDefaultParams()
+    trainClass.setCost(1000)
+    model4 = trainClass.train()
+    pLabel4, pAcc4, pVal4 = trainClass.predict(model4)
+    print("The accuracy of the forth model (C = 1000 and kernelType = 2) is {0}%.".format(pAcc4[0]))
 
-# 4. 将 Cost = 1000 然后使用 RBF kernel
-trainClass.setDefaultParams()
-trainClass.setCost(1000)
-model4 = trainClass.train()
-pLabel4, pAcc4, pVal4 = trainClass.predict(model4)
-print("The accuracy of the forth model (C = 1000 and kernelType = 2) is {0}%.".format(pAcc4[0]))
+    # 5. 使用easy.py去找超参数
+    # 使用命令: python3 easy.py ../../ExerciseCode_Python/svmguide1.txt ../../ExerciseCode_Python/svmguide1.t
+    # 找到的超参数: c=8.0, g=2.0
+    # 在这里试一下
+    trainClass.setDefaultParams()
+    trainClass.setCost(8)
+    trainClass.setGamma(2)
+    model5 = trainClass.train()
+    pLabel5, pAcc5, pVal5 = trainClass.predict(model5)
+    print("The accuracy of the forth model (C = 8 and gamma = 2 and without scale) is {0}%.".format(pAcc5[0]))
 
-# 5. 使用easy.py去找超参数
-# 使用命令: python3 easy.py ../../ExerciseCode_Python/svmguide1.txt ../../ExerciseCode_Python/svmguide1.t
-# 找到的超参数: c=8.0, g=2.0
-# 在这里试一下
-trainClass.setDefaultParams()
-trainClass.setCost(8)
-trainClass.setGamma(2)
-model5 = trainClass.train()
-pLabel5, pAcc5, pVal5 = trainClass.predict(model5)
-print("The accuracy of the forth model (C = 8 and gamma = 2 and without scale) is {0}%.".format(pAcc5[0]))
-
-trainClassScale.setDefaultParams()
-trainClassScale.setCost(8)
-trainClassScale.setGamma(2)
-model5_ = trainClassScale.train()
-pLabel5_, pAcc5_, pVal5_ = trainClassScale.predict(model5_)
-print("The accuracy of the forth model (C = 8 and gamma = 2 and with sacle) is {0}%.".format(pAcc5_[0]))
+    trainClassScale.setDefaultParams()
+    trainClassScale.setCost(8)
+    trainClassScale.setGamma(2)
+    model5_ = trainClassScale.train()
+    pLabel5_, pAcc5_, pVal5_ = trainClassScale.predict(model5_)
+    print("The accuracy of the forth model (C = 8 and gamma = 2 and with sacle) is {0}%.".format(pAcc5_[0]))
 
 
-'''
-下面是教程中的使用的例子说明 注释掉了
-'''
-'''
-# 读取数据
-# PS: svm_read_problem 还有一个参数是 return_scipy = False 即并不默认返回scipy格式的数据
-yTrain, xTrain = svm_read_problem("./svmguide1.txt")
-yTest, xTest = svm_read_problem("./svmguide1.t")
-# for i in range(len(x)):
-#     print(x[i], y[i])
-'''
+    '''
+    下面是教程中的使用的例子说明 注释掉了
+    '''
+    '''
+    # 读取数据
+    # PS: svm_read_problem 还有一个参数是 return_scipy = False 即并不默认返回scipy格式的数据
+    yTrain, xTrain = svm_read_problem("./svmguide1.txt")
+    yTest, xTest = svm_read_problem("./svmguide1.t")
+    # for i in range(len(x)):
+    #     print(x[i], y[i])
+    '''
 
-'''
-svm_train 的 配置参数说明:
-    1. -s svm_type : 设置svm的类别 (默认是 0)
-        0 -- C-SVC (多类别分类)
-        1 -- nu-SVC (多类别分类)
-        2 -- one-class SVM
-        3 -- epsilon-SVR (回归)
-        4 -- nu-SVR(regression)
-    2. -t kernel_type : 设置核方法类别 (默认是 2)
-        0 -- linear
-        1 -- polynomial
-        2 -- Gaussian
-        3 -- sigmoid
-        4 -- precomputed kernel
-    3. -d degree : 设置核方法中的d参数(应该是多项式) (默认是 3)
-    4. -g gamma : 应该是高斯核和多项式核函数之类的那个在前面的乘的系数 (默认是 1/feature的个数)
-    5. -r coef0 : (应该是多项式核函数的那个括号里的+c) (默认是 0)
-    6. -c cost : 设置C-SVC，epsilon-SVR, nu-SVR 中的参数C(那个加入了松弛条件之后的惩罚项的系数) (默认是 1)    
-    7. -n nu : 设置nu-SVC, one-class SVM, nu-SVR 的参数 nu (默认是 0.5)
-    8. -p epsilon : 设置epsilon-SVR中的 epsilon参数 (默认是 0.1)
-    9. -m cachesize : 设置cache 大小(MB) (默认是 100)
-    10. -e epsilon : 设置终止的条件 (默认是 0.001)
-    11. -h shrinking : 设置是否使用shrinking heuristics, 0 or 1 (默认为 1)
-    12. -b probability_estimates : 是否使用SVC/SVR模型去进行概率估计 0 or 1 (默认为 0)
-    13. -wi weight : 在 C-SVC 中设置类别 i 的参数 C 为 weight * C (默认是 1) (PS: 在处理类别不平衡的数据的时候有用)
-    14. -v n : n-fold cross validation mode (要求 n >= 2)
-    15. -q : quite mode
-'''
-'''
-m = svm_train(yTrain, xTrain, '-c 4') # 因此这里 -c 4 指的是松弛的那个参数
-p_label, p_acc, p_val = svm_predict(yTest, xTest, m)
-print(p_label)
-# p_acc 中包含三个参数: 1. accuracy 2. mean-squared error 3. squared correlation coefficient(for regression)
-# ps: 分类问题的均方误差就是 错误率(因为判断错误的时候得到的误差的平方值总是1)
-print(p_acc)
-print(p_val)
-'''
+    '''
+    svm_train 的 配置参数说明:
+        1. -s svm_type : 设置svm的类别 (默认是 0)
+            0 -- C-SVC (多类别分类)
+            1 -- nu-SVC (多类别分类)
+            2 -- one-class SVM
+            3 -- epsilon-SVR (回归)
+            4 -- nu-SVR(regression)
+        2. -t kernel_type : 设置核方法类别 (默认是 2)
+            0 -- linear
+            1 -- polynomial
+            2 -- Gaussian
+            3 -- sigmoid
+            4 -- precomputed kernel
+        3. -d degree : 设置核方法中的d参数(应该是多项式) (默认是 3)
+        4. -g gamma : 应该是高斯核和多项式核函数之类的那个在前面的乘的系数 (默认是 1/feature的个数)
+        5. -r coef0 : (应该是多项式核函数的那个括号里的+c) (默认是 0)
+        6. -c cost : 设置C-SVC，epsilon-SVR, nu-SVR 中的参数C(那个加入了松弛条件之后的惩罚项的系数) (默认是 1)    
+        7. -n nu : 设置nu-SVC, one-class SVM, nu-SVR 的参数 nu (默认是 0.5)
+        8. -p epsilon : 设置epsilon-SVR中的 epsilon参数 (默认是 0.1)
+        9. -m cachesize : 设置cache 大小(MB) (默认是 100)
+        10. -e epsilon : 设置终止的条件 (默认是 0.001)
+        11. -h shrinking : 设置是否使用shrinking heuristics, 0 or 1 (默认为 1)
+        12. -b probability_estimates : 是否使用SVC/SVR模型去进行概率估计 0 or 1 (默认为 0)
+        13. -wi weight : 在 C-SVC 中设置类别 i 的参数 C 为 weight * C (默认是 1) (PS: 在处理类别不平衡的数据的时候有用)
+        14. -v n : n-fold cross validation mode (要求 n >= 2)
+        15. -q : quite mode
+    '''
+    '''
+    m = svm_train(yTrain, xTrain, '-c 4') # 因此这里 -c 4 指的是松弛的那个参数
+    p_label, p_acc, p_val = svm_predict(yTest, xTest, m)
+    print(p_label)
+    # p_acc 中包含三个参数: 1. accuracy 2. mean-squared error 3. squared correlation coefficient(for regression)
+    # ps: 分类问题的均方误差就是 错误率(因为判断错误的时候得到的误差的平方值总是1)
+    print(p_acc)
+    print(p_val)
+    '''
--- Exercise7_3.py
+++ Exercise7_3.py
@@ -1,7 +1,22 @@
-from .Exercise7_1 import Exercise7_1
-from libsvm import *
+import Exercise7_1
 from scipy import sparse
 import os
+import time
 
-trainClass = Exercise7_1()
-trainClass.loadTestDataFromFile("")
+trainClass = Exercise7_1.Exercise7_1()
+trainClass.loadTestDataFromFile("./test1.txt")
+trainClass.loadTrainDataFromFile("./train1.txt")
+
+# 1. all the superParameters are the default value
+# in the Exercise7_3_Result.txt I have delete all median messages because of the size of the file
+# it's not accurate at all and time-cost
+t1 = time.time()
+model1 = trainClass.train()
+t2 = time.time()
+pLabel1, pAcc1, pVal1 = trainClass.predict(model1)
+t3 = time.time()
+print("The accuracy is {0}%.".format(pAcc1[0]))
+print("The timecost is:\n Training time: {0}, Testing time: {1}".format(t2 - t1, t3 - t2))
+
+# 2. using easy.py
+# It costs such a long time.
