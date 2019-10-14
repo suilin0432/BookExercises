162747cd975a6c26c3f50c1e665cab8bb68c14da
隋霖 <401569798@qq.com>
2019-10-14 11:49:03 +0800

    add Exercise13_3.py

---
+++ Exercise13_3.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+class Exercise13_3(object):
+    def __init__(self):
+        pass
+
+    def draw(self, x, y, color, title):
+        plt.plot()
+
+    def run(self):
+        # Beta(0.5, 0.5)
+        pass
+
+
+obj = Exercise13_3()
+
+
